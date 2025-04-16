3b8cb4037c78ebc6c043f22d60541b5e4df9089f
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2025-04-16 14:58:42 -0400

    ADD: random destructable blocks to map generator

--- packages/client/src/utils/roomLayoutGenerator.ts
+++ packages/client/src/utils/roomLayoutGenerator.ts
@@ -1,12 +1,25 @@
-type roomSlice = (" " | "x")[];
+/**
+ * ' ' = floor
+ * 'x' = indestructable wall
+ * 'c' = destructable wall
+ */
+type roomSlice = (" " | "x" | "c")[];
 
-function roomLayoutGenerator(height = 30, width = 30) {
+function roomLayoutGenerator(
+  height = 30,
+  width = 30,
+  destructableBlocks = true
+) {
   const room: roomSlice[] = [];
   for (let i = 0; i < height; i++) {
     if (i === 0 || i === height - 1) room.push(new Array(width).fill("x"));
-    else if (room[i - 1].slice(1,-1).includes("x"))
+    else if (room[i - 1].slice(1, -1).includes("x"))
       room.push(["x", ...new Array(width - 2).fill(" "), "x"]);
     else if (!room[i]) room.push(widthGenerator(width));
+
+    if (destructableBlocks) {
+      room[i] = addDestructableBlocks(room[i], 0.33);
+    }
   }
 
   return room;
@@ -22,6 +35,13 @@ function widthGenerator(width: number): roomSlice {
 
   return roomSlice;
 }
-roomLayoutGenerator(10, 5);
+
+function addDestructableBlocks(slice: roomSlice, percentage = 0.25): roomSlice {
+  return slice.map((tile) => {
+    if (tile !== " ") return tile;
+    if (Math.random() < percentage) return "c";
+    return tile;
+  });
+}
 
 export default roomLayoutGenerator;
