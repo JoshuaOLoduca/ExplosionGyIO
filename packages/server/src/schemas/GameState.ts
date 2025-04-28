191b280451329219e8e39c01d611c62609430b07
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2025-04-28 15:27:10 -0400

    ADD: explosion entity for bomb detinatition

--- packages/server/src/schemas/GameState.ts
+++ packages/server/src/schemas/GameState.ts
@@ -16,12 +16,20 @@ export class Tile extends BaseTile {
   imageId = "";
 }
 
+export class Explosion extends BaseTile {
+  @type("uint8")
+  damage = 1;
+}
+
 export class Bomb extends BaseTile {
   /**
    * Once it hits 0, it will explode. Currently assuming its miliseconds
    */
   @type("uint16")
   fuse = 10;
+
+  @type({ array: Explosion })
+  explosions = [];
 }
 
 export class Player extends BaseTile {
