87ec2accceb1d479c79bb200ee6476358af4a9a9
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2025-05-01 12:27:40 -0400

    ADD: parant and data tracking for each entity

--- packages/server/src/schemas/GameState.ts
+++ packages/server/src/schemas/GameState.ts
@@ -1,6 +1,18 @@
 import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
 
 export class BaseTile extends Schema {
+  constructor(parent?: BaseTile) {
+    super();
+    this.parent = parent;
+    this.id = (Math.random() * 10000).toFixed(10);
+  }
+
+  parent?: BaseTile;
+
+  id: string;
+
+  data = new Map();
+
   @type("number")
   x = 0;
 
