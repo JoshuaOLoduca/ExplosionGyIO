cbf1bb1e9ba72dbb0c2ee2e3e9520a252407e1e3
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2025-05-06 17:53:57 -0400

    ADD: initial file for box collision

---
+++ packages/server/src/utils/physics/checkBoxCollision.ts
@@ -0,0 +1,11 @@
+import { Tile } from "../../schemas";
+
+export function checkBoxCollision(
+  box: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+  },
+  collisionTiles: Tile[]
+) {}
