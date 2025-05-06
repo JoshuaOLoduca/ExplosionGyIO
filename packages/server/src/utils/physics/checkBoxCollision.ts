2fe81420c54de3a0029c3cff1065db64cc48838a
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2025-05-06 18:09:16 -0400

    ADD: box collision check func

--- packages/server/src/utils/physics/checkBoxCollision.ts
+++ packages/server/src/utils/physics/checkBoxCollision.ts
@@ -1,11 +1,32 @@
 import { Tile } from "../../schemas";
+import { isInsideTile } from "./isInsideTile";
 
+type tCoord = { x: number; y: number };
+
+/**
+ * 
+ * @param box 
+ * @param collisionTiles 
+ * @returns false if array is length 0, otherwise return array of which sides have collisions
+ */
 export function checkBoxCollision(
   box: {
-    top: number;
-    right: number;
-    bottom: number;
-    left: number;
+    top: tCoord;
+    right: tCoord;
+    bottom: tCoord;
+    left: tCoord;
   },
   collisionTiles: Tile[]
-) {}
+) {
+  const loopObj = Object.entries(box) as [keyof typeof box, tCoord][];
+  const foundCollisions: [keyof typeof box, tCoord][] = [];
+
+  for (const tile of collisionTiles) {
+    const collisions = loopObj.filter(([_, coord]) =>
+      isInsideTile(coord.x, coord.y, tile)
+    );
+    if (collisions.length) foundCollisions.push(...collisions);
+  }
+
+  return foundCollisions.length ? foundCollisions : false;
+}
