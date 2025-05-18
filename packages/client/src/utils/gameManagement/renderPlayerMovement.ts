f61d8ec210f8cdc0d78a20d52d1cd09b5e60caf1
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2025-05-18 15:43:06 -0400

    FIX: movement render being called excessive amounts of times

--- packages/client/src/utils/gameManagement/renderPlayerMovement.ts
+++ packages/client/src/utils/gameManagement/renderPlayerMovement.ts
@@ -13,18 +13,26 @@ export function renderPlayerMovement(this: Game) {
       | Phaser.GameObjects.Text
       | undefined;
 
-    const movementAmount = 0.2;
+    const movementAmount = 0.1;
 
-    if (serverX !== entity.serverX) {
-      const newX = Phaser.Math.Linear(entity.x, serverX, movementAmount);
+    if (serverX !== entity.x) {
+      const newX =
+        Math.abs(serverX - entity.x) <= 0.1
+          ? serverX
+          : Phaser.Math.Linear(entity.x, serverX, movementAmount);
+
       entity.x = newX;
       if (entity.data.has("image")) entity.data.get("image").x = newX;
       if (healthHud) healthHud.setX(newX - healthHud.data.get("paddingX"));
       if (usernameHud)
         usernameHud.setX(newX - usernameHud.data.get("paddingX"));
     }
-    if (serverY !== entity.serverY) {
-      const newY = Phaser.Math.Linear(entity.y, serverY, movementAmount);
+    if (serverY !== entity.y) {
+      const newY =
+        Math.abs(serverY - entity.y) <= 0.1
+          ? serverY
+          : Phaser.Math.Linear(entity.y, serverY, movementAmount);
+
       entity.y = newY;
       if (entity.data.has("image")) entity.data.get("image").y = newY;
       if (healthHud) healthHud.setY(newY + healthHud.data.get("paddingY"));
