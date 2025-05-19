b8a11ee53b115a76f93584a59727a8aa4cbd7318
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2025-05-18 22:55:28 -0400

    FIX: linear interp not being linear

--- packages/client/src/utils/gameManagement/renderPlayerMovement.ts
+++ packages/client/src/utils/gameManagement/renderPlayerMovement.ts
@@ -1,5 +1,38 @@
 import { Game } from "../../scenes/Game";
 
+const bigboicache = new Map<
+  string,
+  {
+    serverPoint: number;
+    moves: number[];
+  }
+>();
+
+/**
+ *
+ * Creates a list of points to translate player movement smoothly.
+ * The array is constructed in reverse, this means we can use .pop to get the latest move, which is a o(1) operation.
+ */
+function createMoves(
+  startPoint: number,
+  serverPoint: number,
+  movementAmount: number,
+  key: string
+) {
+  bigboicache.set(key, {
+    serverPoint,
+    moves: new Array(1 / movementAmount)
+      .fill(0)
+      .map((_, index, { length }) =>
+        Phaser.Math.Linear(
+          startPoint,
+          serverPoint,
+          (length - index) * movementAmount
+        )
+      ),
+  });
+}
+
 export function renderPlayerMovement(this: Game) {
   for (let sessionId of this.sessionIds.values()) {
     // interpolate all player entities
@@ -15,11 +48,34 @@ export function renderPlayerMovement(this: Game) {
 
     const movementAmount = 0.1;
 
-    if (serverX !== entity.x) {
-      const newX =
-        Math.abs(serverX - entity.x) <= 0.1
-          ? serverX
-          : Phaser.Math.Linear(entity.x, serverX, movementAmount);
+    /**
+     * Populate linear X movement cache if the state X changes.
+     */
+    if (
+      Number.isFinite(serverX) &&
+      serverX !== entity.x &&
+      (!bigboicache.get(sessionId + "x") ||
+        bigboicache.get(sessionId + "x")?.serverPoint !== serverX)
+    ) {
+      createMoves(entity.x, serverX, movementAmount, sessionId + "x");
+    }
+
+    /**
+     * Populates linear Y moves if the state for y position changes
+     */
+    if (
+      Number.isFinite(serverY) &&
+      serverY !== entity.y &&
+      (!bigboicache.get(sessionId + "y") ||
+        bigboicache.get(sessionId + "y")?.serverPoint !== serverY)
+    ) {
+      createMoves(entity.y, serverY, movementAmount, sessionId + "y");
+    }
+
+    // If there are moves cached for our current destination, start translating the player sprite towards it.
+    const xMovement = bigboicache.get(sessionId + "x");
+    if (xMovement?.moves.length) {
+      const newX = xMovement.moves.pop()!;
 
       entity.x = newX;
       if (entity.data.has("image")) entity.data.get("image").x = newX;
@@ -27,11 +83,11 @@ export function renderPlayerMovement(this: Game) {
       if (usernameHud)
         usernameHud.setX(newX - usernameHud.data.get("paddingX"));
     }
-    if (serverY !== entity.y) {
-      const newY =
-        Math.abs(serverY - entity.y) <= 0.1
-          ? serverY
-          : Phaser.Math.Linear(entity.y, serverY, movementAmount);
+
+    // If there are moves cached for our current destination, start translating the player sprite towards it.
+    const yMovement = bigboicache.get(sessionId + "y");
+    if (yMovement?.moves.length) {
+      const newY = yMovement.moves.pop()!;
 
       entity.y = newY;
       if (entity.data.has("image")) entity.data.get("image").y = newY;
