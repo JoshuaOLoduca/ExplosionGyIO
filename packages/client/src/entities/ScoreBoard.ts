980cd66459e622585fb47d01383212d5ef9eb182
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2025-06-04 12:59:44 -0400

    ADD: typing for super call

--- packages/client/src/entities/ScoreBoard.ts
+++ packages/client/src/entities/ScoreBoard.ts
@@ -1,6 +1,15 @@
-export class ScoreBoard extends Phaser.GameObjects.Container {
-  constructor(scene: Phaser.Scene) {
-    super(scene);
+const pContainer = Phaser.GameObjects.Container;
+type tPContainerConstructor = ConstructorParameters<typeof pContainer>;
+
+export class ScoreBoard extends pContainer {
+  constructor(...superArgs: tPContainerConstructor) {
+    super(...superArgs);
     throw "not Impl";
+  }
+}
+
+class PlayerScore extends pContainer {
+  constructor(...superArgs: tPContainerConstructor) {
+    super(...superArgs);
   }
 }
